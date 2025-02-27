0a10e9266a0811d89fbd32b7dfd56a8f56119f6a
jaocomepedra <162987345+jaocomepedra@users.noreply.github.com>
2025-02-27 11:07:57 -0300

    SEXO45

--- aulaivan/src/aulaivan/Animal.java
+++ aulaivan/src/aulaivan/Animal.java
@@ -1,5 +1,26 @@
-package aulaivan;
+package br.com.petshop;
 
 public class Animal {
+    private String nome;
+    private int idade;
 
+    // Construtor para inicializar o objeto Animal
+    public Animal(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    // Métodos para obter os valores dos atributos
+    public String getNome() {
+        return nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    // Método para exibir informações do animal
+    public void exibirInfo() {
+        System.out.println("Nome: " + nome + ", Idade: " + idade + " anos");
+    }
 }
--- aulaivan/src/aulaivan/Petshop.java
+++ aulaivan/src/aulaivan/Petshop.java
@@ -1,10 +1,61 @@
-package aulaivan;
+package br.com.petshop;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Petshop {
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in);
+		ArrayList<Animal> animais = new ArrayList<>();
+		int opcao;
+		
+		do {
+			System.out.println("\n=== MENU ===");
+			System.out.println("1 - Cadastrar Cachorro");
+			System.out.println("2 - Cadastrar Gato");
+			System.out.println("3 - Exibir Animais");
+			System.out.println("4 - Sair");
+			System.out.print("Escolha uma opção: ");
+			opcao = scanner.nextInt();
+			scanner.nextInt();
+		switch (opcao) {
+		case 1:
+			System.out.print("Nome do cachorro:");
+			String nomecachorro = scanner.nextLine();
+			System.out.print("Idade do Cachorro:");
+			int idadeCachorro = scanner.nextInt();
+			scanner.nextLine();
+			System.out.print("Raça do cachorro");
+			String raca = scanner.nextLine();
+			animais.add(new Cachorro(nomecachorro, idadeCachorro, raca));
+			System.out.println("Cachorro cadastrado com sucesso!");
+			break;
+			
+		case 2:
+			System.out.println("Nome Do Gato: ");
+			String nomeGato = scanner.nextLine();
+			System.out.println("Idade do Gato");
+			int idadeGato = scanner.nextInt();
+			System.out.println("cor do pelo: ");
+		    String corPelo = scanner.nextLine();
+			scanner.nextLine();
+			animais.add(new Gato(nomeGato, idadeGato, corPelo));
+			System.out.println("Gato cadastrado com sucesso!");
+			break;
+			
+		case 3:
+			System.out.println("\nLista de Animais");
+			for(Animal animal : animais) {
+				animal.exibirInfo();
+				
+			}
+			break;
+		case 4:
+			System.out.println("Encerrando o programa");
+			break;
+			default:
+				System.out.println("opção inválida! tente novamente:");
+			}
+		} while (opcao != 4);
 	}
-
 }
